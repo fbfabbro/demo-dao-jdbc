ff902e8016d25427770ac893a6a63b39df1b9c4a
fernando bonilha dal fabbro <fbfabbro@gmail.com>
2020-09-20 12:46:21 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
-		
 		SellerDao sellerDao = Daofactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(3);
+		
+		
 		System.out.println(seller);
 		
 	}
