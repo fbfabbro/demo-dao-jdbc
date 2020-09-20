f7e393f1d705c5ecd1ea2e838aee83df08fb97a7
fernando bonilha dal fabbro <fbfabbro@gmail.com>
2020-09-20 11:33:07 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.Daofactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+		
+		SellerDao sellerDao = Daofactory.createSellerDao();
 		System.out.println(seller);
 		
 	}
